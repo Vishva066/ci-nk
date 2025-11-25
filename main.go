412db0ef2c81bfeb34001501f706e408c7d3e439
Vishva <vishva.prasad@duk.ac.in>
2025-11-25 17:31:16 +0530

    Trial4

--- main.go
+++ main.go
@@ -15,6 +15,6 @@ func main() {
 	port := "8080"
 	fmt.Printf("Server listening on port %s...\n", port)
 	if err := http.ListenAndServe(":"+port, nil); err != nil {
-		fmt.Printf("Error starting server in golang: %s\n", err)
+		fmt.Printf("Error starting server in go: %s\n", err)
 	}
 }
